0c122cf3aef34e4d824c1109c56474816e41a5bc
Sky <31314981+AlyssaXD@users.noreply.github.com>
2017-09-29 09:18:47 -0500

    Add files via upload

---
+++ BankBalance.java
@@ -0,0 +1,29 @@
+package Tutorials;
+
+import java.util.Scanner;
+
+public class BankBalance 
+{
+
+	public static void main(String[] args) 
+	{
+		int choice;
+		double balance;
+		int year = 0;
+			final double INT_RATE = 0.08;
+			Scanner keyboard = new Scanner(System.in);
+			System.out.print("Enter initial bank balance > ");
+			balance = keyboard.nextDouble();
+			
+			do {
+				year = year +1;
+				balance = balance + balance * INT_RATE;
+				System.out.println("Your new balance with interest is " + balance + " year " + year);
+				
+				System.out.println("Would you like to see next years balance? 1 for yes 2 for no.");
+				choice = keyboard.nextInt();
+				
+			}while(choice == 1);
+	}
+
+}
---
+++ FixDebugSix4.java
@@ -0,0 +1,40 @@
+package Debugging;
+
+import java.util.Scanner;
+
+public class FixDebugSix4 
+{
+
+	public static void main(String[] args) 
+	{
+		int high, low, count = 0;
+		final int NUM = 5;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("This application displays " + NUM + 
+				" random numbers" + 
+				"\nbetween the low and high values you enter" + 
+				"\nEnter low value now... ");
+		high = input.nextInt();
+		System.out.print("Enter high value... ");
+		low = input.nextInt();
+		while(low >= count);
+		{
+			System.out.println("The number you entered for a high number, " +
+					high + ", is not more than " + low);
+			System.out.print("Enter a number higher than " + low + "...");
+			high = input.nextInt();
+		}
+		while(count <= NUM);
+		{
+			double result = Math.random();
+			int answer = (int) (result * 10 + low);
+			if(answer <= high)
+			{
+				System.out.print(answer + " ");
+				count = count - 1;
+			}
+		}
+	}
+
+}
